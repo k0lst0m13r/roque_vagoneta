4e0567449c2adc4af8d3f75ceacc2566523b5676
Julio Branda <kolstomier@protonmail.com>
2022-07-25 01:23:44 -0300

    iniciado proyecto, creada app, views, urls y templates provisorias

--- roque_vagoneta/roque_vago_app/views.py
+++ roque_vagoneta/roque_vago_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'roque_vago_app/home.html')
+
+def tienda(request):
+    return render(request,'roque_vago_app/tienda.html')
+
+def contacto(request):
+    return render(request,'roque_vago_app/contacto.html')
