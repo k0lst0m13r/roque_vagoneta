04684dd5014b9aa7ba86396d779725da7eb647c8
Julio Branda <kolstomier@protonmail.com>
2022-07-31 18:39:29 -0300

    hecha migracion de DB y modelo de Aeticulos

--- roque_vagoneta/roque_vago_app/models.py
+++ roque_vagoneta/roque_vago_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    categoria = models.CharField(max_lengt h=50)
+    imagen = models.ImageField(upload_to='articulos')
+
+    class Meta:
+        verbose_name = 'Servicio'
+        verbose_name_plural = 'Servicios'
+
+    def __str__(self):
+        return self.nombre
