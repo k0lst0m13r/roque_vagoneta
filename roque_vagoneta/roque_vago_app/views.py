0ced49cece00495ff6bb5b2d41d82e2f8e40ee57
Julio Branda <kolstomier@protonmail.com>
2022-07-31 20:52:03 -0300

    vista articulos en template 'Tienda' OK!

--- roque_vagoneta/roque_vago_app/views.py
+++ roque_vagoneta/roque_vago_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from roque_vago_app.models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'roque_vago_app/index.html')
 
 def tienda(request):
-    return render(request,'roque_vago_app/tienda.html')
+    articulos = Articulo.objects.all()
+    ctx = {'articulos': articulos}
+    return render(request,'roque_vago_app/tienda.html', ctx)
 
 def contacto(request):
     return render(request, 'roque_vago_app/contacto.html')
