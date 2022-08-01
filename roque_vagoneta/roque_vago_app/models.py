283e422bdbf361dc7c45941c858936928c21b6bc
Julio Branda <kolstomier@protonmail.com>
2022-07-31 21:32:01 -0300

    agregado precio al model Articulo

--- roque_vagoneta/roque_vago_app/models.py
+++ roque_vagoneta/roque_vago_app/models.py
@@ -6,6 +6,7 @@ class Articulo(models.Model):
     descripcion = models.CharField(max_length=200)
     categoria = models.CharField(max_length=50)
     imagen = models.ImageField(upload_to='articulos')
+    precio = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
 
     class Meta:
         verbose_name = 'Articulo'
