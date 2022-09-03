3ac3a391bf508931e006b7a8a60bf9553f275d61
Julio Branda <kolstomier@protonmail.com>
2022-09-03 13:23:23 -0300

    migrados los modelos a la app tienda

--- roque_vagoneta/tienda/admin.py
+++ roque_vagoneta/tienda/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Reposteria)
+admin.site.register(DisfracesYaccesorios)
+admin.site.register(Descartables)
+admin.site.register(Decoracion)
+admin.site.register(Cumpleanios)
