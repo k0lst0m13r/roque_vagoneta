b1778b2f9b5d910bb7ef8c5b7d601a51baef52d3
Julio Branda <kolstomier@protonmail.com>
2022-10-17 13:13:39 -0300

    ya me perdi

--- roque_vagoneta/carro/views.py
+++ roque_vagoneta/carro/views.py
@@ -4,6 +4,26 @@ from tienda.models import *
 
 # Create your views here.
 
-def agregar_articulo(request, articulo_id):
+def agregar_reposteria(request, articulo_id):
     carro = Carro(request)
-    
+    articulo = Reposteria.objects.get(id=articulo_id):
+    carro.agregar_articulo(articulo=articulo)
+    return redirect("tienda")
+
+def agregar_disfraces(request, articulo_id):
+    carro = Carro(request)
+    articulo = DisfracesYaccesorios.objects.get(id=articulo_id):
+    carro.agregar_articulo(articulo=articulo)
+    return redirect("tienda")
+
+def agregar_descartables(request, articulo_id):
+    carro = Carro(request)
+    articulo = Descartables.objects.get(id=articulo_id):
+    carro.agregar_articulo(articulo=articulo)
+    return redirect("tienda")
+
+def agregar_deoracion(request, articulo_id):
+    carro = Carro(request)
+    articulo = Decoracion.objects.get(id=articulo_id):
+    carro.agregar_articulo(articulo=articulo)
+    return redirect("tienda")
