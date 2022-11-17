1e72548ab8afa8acb8ed96bd95fa8013a5af9bd7
Julio Branda <kolstomier@protonmail.com>
2022-11-17 13:54:52 -0300

    modificados: models, views y admin. ahora solo quedan dos modelos, "Categoria" y "Articulo "

--- roque_vagoneta/tienda/views.py
+++ roque_vagoneta/tienda/views.py
@@ -4,16 +4,7 @@ from tienda.models import *
 # Create your views here.
 def tienda(request):
 
-    reposteria = Reposteria.objects.all()
+    articulos = Articulo.objects.all()
 
-    disfraces = DisfracesYaccesorios.objects.all()
-
-    descartables = Descartables.objects.all()
-
-    decoracion = Decoracion.objects.all()
-
-    cumple = Cumpleanios.objects.all()
-
-
-    ctx = {'cumple': cumple, 'reposteria': reposteria, 'disfraces': disfraces, 'descartables': descartables, 'decoracion': decoracion,}
+    ctx = {"articulos": articulos}
     return render(request,'tienda/tienda.html', ctx)
