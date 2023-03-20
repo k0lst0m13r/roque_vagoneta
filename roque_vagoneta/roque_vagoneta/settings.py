17054e09c7690ee7c70bab5f2e0db293604175ef
Julio Branda <kolstomier@protonmail.com>
2023-03-19 22:26:24 -0300

    SECRET_KEY a la variable de entorno

--- roque_vagoneta/roque_vagoneta/settings.py
+++ roque_vagoneta/roque_vagoneta/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kc)+obqu6_c9rag54mf4+jfwz(df4b0f)*aoy5233!kut3(lf4'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
