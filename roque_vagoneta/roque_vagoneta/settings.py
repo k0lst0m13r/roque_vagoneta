c2b9035691f2027565e1d178d2ac98cb5b661e88
Julio Branda <kolstomier@protonmail.com>
2023-04-27 18:43:08 -0300

    preparando para pythonanywhere

--- roque_vagoneta/roque_vagoneta/settings.py
+++ roque_vagoneta/roque_vagoneta/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://roquevagoneta.vercel.app/"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
